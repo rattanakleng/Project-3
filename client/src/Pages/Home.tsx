b913f21dd1d6ed5c7a2c3ccc891dae73bd2e26f2
Rattanak Leng <rleng@live.com>
2021-04-01 09:13:22 -0700

    Initialize BACKUP

---
+++ client/src/Pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero/Hero'
+import HomePageContent from '../components/HomePageContent/HomePageContent'
+
+const Home = () => {
+  return (
+
+    <>
+    <Hero />
+    <HomePageContent />
+    </>
+     
+  )
+}
+
+export default Home
