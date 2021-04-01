eea72609aa12b5845cf871d249ae3046eb9dc927
Rattanak Leng <rleng@live.com>
2021-04-01 09:13:28 -0700

    Initialize fixauth_AEO

---
+++ client/src/components/Hero/Hero.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { ButtonPrimary } from '../ButtonPrimary/ButtonPrimary'
+import './Hero.css'
+
+export const Hero = () => {
+  return (
+    <div className="hero-container row hero">
+      <div className="col-lg-6 col-sm-12 justify-content-center">
+        <div className="d-flex flex-column">
+          <h1 className="text-center"> The largest community</h1>
+          <h1 className="text-center"> of art enthusiasts</h1>
+
+          <Link to="/signup" className="mx-auto mt-4" >
+            <div>
+              <ButtonPrimary text={'Join Today'} />
+            </div>
+          </Link>
+
+        </div>
+      </div>
+
+      <div className="col-lg-6 col-sm-12 ">
+        <img
+          src="./assets/img/watercolor-5049980_1920.jpg"
+          className="hero-img"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
