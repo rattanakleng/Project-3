3972b3f2bcd173335bfb35a48839271fa1df6fe9
Rattanak Leng <rleng@live.com>
2021-04-01 09:13:17 -0700

    Initialize create-nav-bar-comp

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Button from './components/shared/Button/Button';
+
+
+function App() {
+  return (
+    <Button />
+  );
+}
+
+export default App;
