2876af9f11a698e5dc6d0848691def00c14fe676
Rattanak Leng <rleng@live.com>
2021-04-01 09:13:19 -0700

    Initialize MongoConnection_AEO

---
+++ client/src/Pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero/Hero'
+import HomePageMain from '../components/HomePageMain/HomePageMain'
+
+const Home = () => {
+  return (
+
+    <>
+    <Hero />
+    <HomePageMain />
+    </>
+     
+  )
+}
+
+export default Home
