44c17455c3d7f68cad9c1658b06697d16ff43e07
Rattanak Leng <rleng@live.com>
2021-04-01 09:13:29 -0700

    Initialize heroku_games_AEO

---
+++ client/src/Pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../components/Hero/Hero'
+// import HomePageContent from '../components/HomePageContent/HomePageContent'
+// HomePageContent contains search bars, future dev
+
+const Home = () => {
+  return (
+
+    <>
+    <Hero />
+    {/* <HomePageContent /> */}
+    </>
+     
+  )
+}
+
+export default Home
